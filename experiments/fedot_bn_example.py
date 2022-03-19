5183014ac572585de01ebe55fd8adf8cb80106be
Dreamlone <mik_sar@mail.ru>
2022-03-19 19:48:18 +0300

    fix integration & example update

--- experiments/fedot_bn_example.py
+++ experiments/fedot_bn_example.py
@@ -1,6 +1,8 @@
 import sys
+from typing import Optional, Union, List
+
 parentdir = 'C:\\Users\\Worker1\\Documents\\BAMT'
-sys.path.insert(0,parentdir)
+sys.path.insert(0, parentdir)
  
 import pandas as pd
 import random
@@ -14,7 +16,7 @@ import bamt.Networks as Nets
  
 from fedot.core.composer.gp_composer.gp_composer import PipelineComposerRequirements
 from fedot.core.dag.validation_rules import has_no_cycle, has_no_self_cycled_nodes
-from fedot.core.log import default_log
+from fedot.core.log import default_log, Log
 from fedot.core.optimisers.adapters import DirectAdapter
 from fedot.core.optimisers.gp_comp.gp_optimiser import EvoGraphOptimiser, GPGraphOptimiserParameters, \
     GeneticSchemeTypesEnum
@@ -26,14 +28,23 @@ from fedot.core.optimisers.optimizer import GraphGenerationParams
 from fedot.core.pipelines.convert import graph_structure_as_nx_graph
 from pgmpy.models import BayesianModel
 from pgmpy.estimators import K2Score
- 
+
+
 # кастомный граф
 class CustomGraphModel(OptGraph):
+
+    def __init__(self, nodes: Optional[Union[OptNode, List[OptNode]]] = None,
+                 log: Log = None):
+        super().__init__(nodes, log)
+        # TODO find out is this field necessary or not - fix it
+        self.unique_pipeline_id = 1
+
     def evaluate(self, data: pd.DataFrame):
         nodes = data.columns.to_list()
         _, labels = graph_structure_as_nx_graph(self)
         return len(nodes)
- 
+
+
 # кастомные узлы
 class CustomGraphNode(OptNode):
     def __str__(self):
@@ -115,12 +126,12 @@ def custom_mutation(graph: OptGraph, **kwargs):
         graph.log.warn(f'Incorrect connection: {ex}')
     return graph
  
- 
+
 # главная функция, которая последовательно запускается
 def run_example():
  
     # Импорт данных и дискретизация
-    data=pd.read_csv(r'C:\\Users\\Worker1\\Documents\\BAMT\\Data\\hack_processed_with_rf.csv')
+    data=pd.read_csv(r'hack_processed_with_rf.csv')
     nodes_types = ['Tectonic regime', 'Period', 'Lithology',
                     'Structural setting', 'Gross', 'Netpay',
                     'Porosity', 'Permeability', 'Depth']
@@ -129,7 +140,6 @@ def run_example():
     discretizer = preprocessing.KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='quantile')
     p = pp.Preprocessor([('encoder', encoder), ('discretizer', discretizer)])
     discretized_data, est = p.apply(data)
-    #node_type = p.info['types']
     global node_type
     node_type=dict(('Node_'+key, value) for (key, value) in p.info['types'].items())
  
@@ -139,36 +149,33 @@ def run_example():
     # изициализация графа без связей, только с узлами
     initial = [CustomGraphModel(nodes=[CustomGraphNode(nodes_from=None,
                                                       content={'name': node_type}) for node_type in nodes_types])]
-   
-   
-   
  
- # параметры ГА
+    # параметры ГА
     requirements = PipelineComposerRequirements(
         primary=nodes_types,
         secondary=nodes_types, max_arity=100,
         max_depth=100, pop_size=10, num_of_generations=50,
         crossover_prob=0.8, mutation_prob=0.9)
  
-# Ещё параметры для ГА
-# genetic_scheme_type -> [steady_state, generational, parameter_free]
-# crossover_types -> [subtree, one_point, none]
-# mutation_types -> [simple, reduce, growth, local_growth] MutationTypesEnum
-# selection_types -> [tournament,nsga2, spea2] SelectionTypesEnum
+    # Ещё параметры для ГА
+    # genetic_scheme_type -> [steady_state, generational, parameter_free]
+    # crossover_types -> [subtree, one_point, none]
+    # mutation_types -> [simple, reduce, growth, local_growth] MutationTypesEnum
+    # selection_types -> [tournament,nsga2, spea2] SelectionTypesEnum
     optimiser_parameters = GPGraphOptimiserParameters(
         genetic_scheme_type=GeneticSchemeTypesEnum.steady_state,
-# добавила селекцию турниром        
+    # добавила селекцию турниром
         selection_types=[SelectionTypesEnum.tournament],
         mutation_types=[custom_mutation],
         crossover_types=[CrossoverTypesEnum.none],
         regularization_type=RegularizationTypesEnum.none)
  
-# Параметры для генерации графов. Закидываем сюда граф, узлы и правила
+    # Параметры для генерации графов. Закидываем сюда граф, узлы и правила
     graph_generation_params = GraphGenerationParams(
         adapter=DirectAdapter(base_graph_class=CustomGraphModel, base_node_class=CustomGraphNode),
         rules_for_constraint=rules)
  
-# Эволюционный оптимизатор графов  
+    # Эволюционный оптимизатор графов
     optimiser = EvoGraphOptimiser(
         graph_generation_params=graph_generation_params,
         metrics=[],
@@ -176,46 +183,27 @@ def run_example():
         requirements=requirements, initial_graph=initial,
         log=default_log(logger_name='Bayesian', verbose_level=1))
  
-# в partial указываем целевую функцию?
+    # в partial указываем целевую функцию?
     optimized_graph = optimiser.optimise(partial(k2_metric, data=discretized_data))
-# Сейчас optimized_graph хранит: {'depth': 9, 'length': 9, 'nodes': [Period, Netpay, ...]}
+    # Сейчас optimized_graph хранит: {'depth': 9, 'length': 9, 'nodes': [Period, Netpay, ...]}
  
-# Отличается от optimized_graph только добавлением 'Node_' к названиям узлов
+    # Отличается от optimized_graph только добавлением 'Node_' к названиям узлов
     optimized_network = optimiser.graph_generation_params.adapter.restore(optimized_graph)
-   
-#    print(optimized_graph)
-    # OF=k2_metric(optimized_graph, data=discretized_data)
-    # print(OF)
-#    print(graph_structure_as_nx_graph(optimized_graph))
+
    
     optimized_graph.show() 
-    return(opt_graph_to_bamt(optimized_network))
-    
-   
-    #optimized_graph.show(path='C:/Users/Worker1/Documents/BAMT/V1.png')
- 
-    #pdf.add_page()
-    #pdf.set_font("Arial", size = 15)
-    #pdf.cell(200, 10, txt = "K2_score = " + str(round(OF[0],2)),
-    #        ln = 1, align = 'C')
-    #pdf.image('C:/Users/Worker1/Documents/BAMT/V1.png',w=200, h=200)
-     
- 
- 
- 
- 
+    return opt_graph_to_bamt(optimized_network)
+
+
 if __name__ == '__main__':
-    #from fpdf import FPDF
-    #pdf = FPDF()
- 
-    data = pd.read_csv(r'C:\\Users\\Worker1\\Documents\\BAMT\\Data\\hack_processed_with_rf.csv')
+    data = pd.read_csv(r'hack_processed_with_rf.csv')
     nodes_types = ['Tectonic regime', 'Period', 'Lithology',
                     'Structural setting', 'Gross', 'Netpay',
                     'Porosity', 'Permeability', 'Depth']
     data = data[nodes_types]
     structure = run_example()
    
-    bn = Nets.HybridBN(has_logit = True, use_mixture=True)
+    bn = Nets.HybridBN(has_logit=True, use_mixture=True)
     encoder = preprocessing.LabelEncoder()
     discretizer = preprocessing.KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='quantile')
     p = pp.Preprocessor([('encoder', encoder), ('discretizer', discretizer)])
@@ -233,16 +221,3 @@ if __name__ == '__main__':
     print(bn.nodes)
     print(bn.edges)
     print(bn.get_info())
-    #bn.fit_parameters(data)
-    #bn.get_params_tree('fedon_bn.json')
-    # sample = bn.sample(100)
-    # sns.displot(data['Netpay'])
-    # sns.displot(sample['Netpay'])
- 
- 
- 
- 
- 
-    #pdf.output("GFG.pdf")
- 
-
